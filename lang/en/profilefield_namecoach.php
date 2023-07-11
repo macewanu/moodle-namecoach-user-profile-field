784597266a6de2f4593d48ad79bd90aedd5405a9
Erwin Veugelers <veugelerse@macewan.ca>
2023-07-11 15:46:15 -0600

    Shorten default no-recording message. Version bump.

--- lang/en/profilefield_namecoach.php
+++ lang/en/profilefield_namecoach.php
@@ -32,4 +32,4 @@ $string['api_token'] = 'NameCoach API token';
 $string['access_code'] = 'Name page access code';
 $string['api_endpoint'] = 'NameCoach API endpoint URL';
 $string['msg_unavailable'] = 'This function is currently unavailable';
-$string['msg_norecording'] = 'No name recording available';
+$string['msg_norecording'] = 'No recording available';
